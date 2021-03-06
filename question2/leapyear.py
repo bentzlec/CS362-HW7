c4d111a42da7cca28ac3a856cae9aacad6a58eab
Connor Bentzley <bentzlec@os1.engr.oregonstate.edu>
2021-03-06 09:54:20 -0800

    Wrote functions to test for multiples of 4, 100, and 400. Also wrote tests for these functions

---
+++ question2/leapyear.py
@@ -0,0 +1,17 @@
+def div_by_4(i):
+   if (i % 4 == 0):
+      return 1
+   else:
+      return 0
+
+def div_by_100(i):
+   if (i % 100 == 0):
+      return 1
+   else:
+      return 0
+
+def div_by_400(i):
+   if (i % 400 == 0):
+      return 1
+   else:
+      return 0
---
+++ question2/test_leapyear.py
@@ -0,0 +1,19 @@
+import leapyear
+import unittest
+
+class TestCase(unittest.TestCase):
+   def test1_div_by_4(self):
+      self.assertEqual(leapyear.div_by_4(16), 1)
+   def test2_div_by_4(self):
+      self.assertEqual(leapyear.div_by_4(10), 0)
+   def test1_div_by_100(self):
+      self.assertEqual(leapyear.div_by_100(400), 1)
+   def test2_div_by_100(self):
+      self.assertEqual(leapyear.div_by_100(372), 0)
+   def test1_div_by_400(self):
+      self.assertEqual(leapyear.div_by_400(1600), 1)
+   def test2_div_by_400(self):
+      self.assertEqual(leapyear.div_by_400(432), 0)
+
+if __name__ == '__main__':
+   unittest.main()
