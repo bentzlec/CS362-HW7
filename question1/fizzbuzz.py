5f19a76dd296e98beeb3ab64361cb849bc161419
Connor Bentzley <bentzlec@os1.engr.oregonstate.edu>
2021-03-06 09:20:05 -0800

    Wrote and tested two functions for checking multiples of three and five

--- question1/fizzbuzz.py
+++ question1/fizzbuzz.py
@@ -2,3 +2,33 @@ def increment(i):
    return i + 1
 
 
+def printNum(i):
+   print(i)
+
+
+def check_multiple_of_3(i):
+   isMultiple = 0
+   if (i % 3 == 0):
+      isMultiple = 1
+      return isMultiple
+   else:
+      #nothing
+      return isMultiple 
+
+
+def check_multiple_of_5(i):
+   isMultiple = 0
+   if (i % 3 == 0):
+      isMultiple = 1
+      return isMultiple
+   else:
+      #nothing
+      return isMultiple
+
+
+
+num = 0
+
+while num <= 100:
+   printNum(num)
+   num = increment(num)
--- question1/test_fizzbuzz.py
+++ question1/test_fizzbuzz.py
@@ -4,6 +4,14 @@ import fizzbuzz
 class TestCase(unittest.TestCase):
    def test_Increment(self):
       self.assertEqual(fizzbuzz.increment(13), 14)
+   def test_Mult_3_1(self):
+      self.assertEqual(fizzbuzz.check_multiple_of_3(9), 1)
+   def test_Mult_3_2(self):
+      self.assertEqual(fizzbuzz.check_multiple_of_3(4), 0)
+   def test_Mult_5_1(self):
+      self.assertEqual(fizzbuzz.check_multiple_of_5(15), 1)
+   def test_Mult_5_2(self):
+      self.assertEqual(fizzbuzz.check_multiple_of_5(16), 0)
 
 if __name__ == '__main__':
    unittest.main()
